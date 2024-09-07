5e98d4511136374cec8517dcd9cc126414b3a8c6
Muhammad Talha <talha@talha-virtua-machine>
2024-09-07 05:19:26 +0100

    Modifying bash_deploy.sh

--- bash_deploy.sh
+++ bash_deploy.sh
@@ -1,1 +1,15 @@
-this is bash
+#!/bin/bash
+echo "Auto deployment"
+if [ ! -d "/home/talha/lab6/bash_deploy.sh/" ]; then
+    cd /home/talha/lab6/
+    echo "Cloning the repository."
+    git clone https://github.com/muhammadtalha766/test.git
+    cd lab6/
+else
+    echo "Repository already cloned. Pulling the latest changes."
+    cd /home/maham/lab6/
+    git pull origin master
+fi
+echo "Moving file into default directory."
+sudo cp * /var/www/html/
+echo "## End ##"
