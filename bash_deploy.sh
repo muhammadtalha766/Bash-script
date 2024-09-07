5b8ffa74a030665a44d4056735af751a956213ca
muhammadtalha766 <talhaiac033@gmail.com>
2024-09-06 21:29:18 -0700

    Update bash_deploy.sh

--- bash_deploy.sh
+++ bash_deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "Auto deployment"
 if [ ! -d "/home/talha/lab6/" ]; then
-    cd /home/talha/lab6/
-    echo "Cloning the repository."
-    git clone https://github.com/muhammadtalha766/test.git
-    cd lab6/
+    cd /home/talha/lab6/    
 else
     echo "Repository already cloned. Pulling the latest changes."
     cd /home/talha/lab6/
     git pull origin master
 fi
+echo "Cloning the repository."
+git clone https://github.com/muhammadtalha766/test.git
+cd lab6/
 echo "Moving file into default directory."
 sudo cp * /var/www/html/
 echo "## End ##"
