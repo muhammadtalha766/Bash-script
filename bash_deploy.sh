95d05d11df92443488f2a6b9d45f025cbeef1f98
Muhammad Talha <talha@talha-virtua-machine>
2024-09-07 05:39:49 +0100

    modifing bash

--- bash_deploy.sh
+++ bash_deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 echo "Auto deployment"
+
+# Check if the lab6 directory exists
 if [ ! -d "/home/talha/lab6/" ]; then
-    cd /home/talha/lab6/    
+    echo "Cloning the repository."
+    git clone https://github.com/muhammadtalha766/test.git /home/talha/lab6/
+    cd /home/talha/lab6/
 else
     echo "Repository already cloned. Pulling the latest changes."
     cd /home/talha/lab6/
-    git pull origin master
+    git pull origin main  # Replace 'master' with 'main'
 fi
-echo "Cloning the repository."
-git clone https://github.com/muhammadtalha766/test.git
-cd lab6/
-echo "Moving file into default directory."
-sudo cp * /var/www/html/
+
+# Move files into the default directory
+echo "Moving files into the default directory."
+sudo cp -r * /var/www/html/
+
 echo "## End ##"
+
