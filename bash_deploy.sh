5e61796485023109f5f30b893a175c8c287da1dd
muhammadtalha766 <talhaiac033@gmail.com>
2024-09-06 21:52:09 -0700

    Update bash_deploy.sh

--- bash_deploy.sh
+++ bash_deploy.sh
@@ -2,13 +2,14 @@
 echo "Auto deployment"
 
 # Check if the lab6 directory exists
-if [ ! -d "/home/talha/lab6/" ]; then
-    echo "Cloning the repository."
-    git clone https://github.com/muhammadtalha766/test.git /home/talha/lab6/
+if [ ! -d "/home/talha/lab6/test/" ]; then
     cd /home/talha/lab6/
+    echo "Cloning the repository."
+    git clone https://github.com/muhammadtalha766/test.git
+    cd /home/talha/lab6/test/
 else
     echo "Repository already cloned. Pulling the latest changes."
-    cd /home/talha/lab6/
+    cd /home/talha/lab6/test/
     git pull origin main  # Replace 'master' with 'main'
 fi
 
